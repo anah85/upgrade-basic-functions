23cbc009f88d056b86bd35ff47f4772cf19c13a0
Ana Hartmann <ana.hartmann@protonmail.com>
2022-12-11 12:42:10 +0100

    first Iteración #2

---
+++ 2Buscar.js
@@ -0,0 +1,16 @@
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+
+
+function findLongestWord(param) {
+    let masLetras = "0";
+
+    for(i=0; i < param.length; i++){
+        if(param[i].length > masLetras.length){
+            masLetras = param[i]
+        }
+    }
+    console.log(masLetras);
+
+    }
+
+    findLongestWord(avengers);
