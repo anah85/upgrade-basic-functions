301a0025e4fbb4e95b1506150642c912d104967d
Ana Hartmann <ana.hartmann@protonmail.com>
2023-01-09 21:12:01 +0100

    Iteración #2

--- 2Buscar.js
+++ 2Buscar.js
@@ -5,12 +5,12 @@ function findLongestWord(param) {
     let masLetras = "0";
 
     for(i=0; i < param.length; i++){
-        if(param[i].length > masLetras.length){
+        if(param[i].length >= masLetras.length){
             masLetras = param[i]
         }
     }
     console.log(masLetras);
-
+        return masLetras;
     }
 
     findLongestWord(avengers);
