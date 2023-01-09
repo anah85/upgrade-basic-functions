7aef9e111aa01e89b3f202b07e090520afdb4744
Ana Hartmann <ana.hartmann@protonmail.com>
2023-01-09 21:15:32 +0100

    Iteración #6

--- 6Valores.js
+++ 6Valores.js
@@ -1,7 +1,7 @@
 /*Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que 
 existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función: */
 
-/*const duplicates = [
+const duplicates = [
     'sushi',
     'pizza',
     'burger',
@@ -14,17 +14,18 @@ existan los elimina para retornar un array sin los elementos duplicados. Puedes 
     'pasta',
     'soda'
   ];
-  function removeDuplicates(array) {
-    let duplicado = [];
-    for (let i = 0; i < duplicates.length; i++) {
-      if (duplicates[i + 1] === duplicates[i]) {
-        duplicado.push(duplicates[i]);
+  function removeDuplicates(duplicates) {
+    for (i = 0; i < duplicates.length; i++) {
+      const duplicado = duplicates[i];
+      console.log('Está repetido  '+ duplicado);
+      let palabra = false;
+    for(j=0; j< duplicates.length; j++){
+      const repetido = duplicates[j];
+      if(duplicado==repetido && i!==j){
+      palabra = true;
       }
     }
+  console.log(palabra.indexof === true);
   }
-    console.log(duplicado);
-*/
-    
-for(i=0;i<=10;i++){
-console.log(i);
 }
+  removeDuplicates(duplicates);
